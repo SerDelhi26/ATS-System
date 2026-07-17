753b44df318d7269a4c7d7c8c06708d6a694ebe9
Delhi Server <serverdelhi1point@outlook.com>
2026-07-17 12:06:21 +0530

    Added Streamlit secrets support

--- db.py
+++ db.py
@@ -1,11 +1,21 @@
 from supabase import create_client
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = (
+    st.secrets["SUPABASE_URL"]
+    if "SUPABASE_URL" in st.secrets
+    else os.getenv("SUPABASE_URL")
+)
+
+SUPABASE_KEY = (
+    st.secrets["SUPABASE_KEY"]
+    if "SUPABASE_KEY" in st.secrets
+    else os.getenv("SUPABASE_KEY")
+)
 
 supabase = create_client(
     SUPABASE_URL,
